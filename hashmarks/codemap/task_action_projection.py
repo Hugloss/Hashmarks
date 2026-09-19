a71262a50d1a1463dc6c024f71106214f99086e4
Hugo Labraaten <hugokinner@gmail.com>
2026-09-19 20:32:20 +0200

    Keep projection sequence typing after extraction

--- hashmarks/codemap/task_action_projection.py
+++ hashmarks/codemap/task_action_projection.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from collections.abc import Mapping
+from collections.abc import Mapping, Sequence
 from copy import deepcopy
 from typing import TYPE_CHECKING, cast
 
