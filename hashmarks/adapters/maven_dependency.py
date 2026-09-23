896c4b7af4d131b235ebb28f89af49fe7841c1d3
Hugo Labraaten <hugokinner@gmail.com>
2026-09-23 17:19:20 +0200

    Parse Maven list module suffix as emitted

--- hashmarks/adapters/maven_dependency.py
+++ hashmarks/adapters/maven_dependency.py
@@ -8,7 +8,7 @@ from collections.abc import Mapping, Sequence
 
 _SCHEMA = "hashmarks.dependency-resolution.v2"
 _LIST_LINE = re.compile(
-    r"^\\s*(?P<coordinate>\\S+)\\s+--\\s+module\\s+(?P<module>.+?)\\s*$"
+    r"^\\s*(?P<coordinate>\\S+)\\s+(?:--\\s+module\\s+)?(?P<module>[^-].*?)\\s*$"
 )
 
 
