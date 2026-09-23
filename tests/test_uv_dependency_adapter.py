c98c491eb3a3cea2d37a9adf0b956b7e7551ee96
Hugo Labraaten <hugokinner@gmail.com>
2026-09-23 19:57:48 +0200

    Format uv adapter regressions

--- tests/test_uv_dependency_adapter.py
+++ tests/test_uv_dependency_adapter.py
@@ -87,7 +87,7 @@ def test_uv_lock_adapter_does_not_promote_declared_metadata_to_resolution() -> N
 
 
 def test_uv_lock_adapter_refuses_ambiguous_name_only_dependency() -> None:
-    lock = b'''version = 1
+    lock = b\"\"\"version = 1
 revision = 3
 requires-python = ">=3.11"
 
